61b3b043ce7d43b4f52d53bcf151120eea6714ae
Timur Badretdinov <accounts@destiner.io>
2024-02-29 20:41:54 +0000

    Tweak Yoink id generation

--- src/Yoink.ts
+++ src/Yoink.ts
@@ -2,7 +2,7 @@ import { ponder } from "@/generated";
 
 ponder.on("Yoink:Yoinked", async ({ event, context }) => {
   await context.db.Yoink.create({
-    id: event.args.yoinker,
+    id: `${event.transaction.hash}-${event.log.logIndex}`,
     data: {
       yoinker: event.args.yoinker,
       txNonce: event.transaction.nonce,
